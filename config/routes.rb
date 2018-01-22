5898b0bdfc7789209571fba7d4dd76312b897b2d
Melany Gulianovych <melanygulianovych@Melanys-Mac-mini.local>
2018-01-22 15:17:07 -0800

    Added controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
     resources :exams, only: [:index]
   end
 
+    namespace :teachers do
+    resources :answers, only: [:index]
+  end
+
   devise_for :users
   root to: 'home#index'
+  get 'home/contact'
+
+  devise_for :answers
+  root to: 'answers#index'
   get 'home/contact'
 end
