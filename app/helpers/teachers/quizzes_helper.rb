3e06242b765645334ff84a6d672c8155adf2a7c9
kmandziy <kmandzii@gmail.com>
2018-01-24 20:27:04 +0200

    Add quizzes

---
+++ app/helpers/teachers/quizzes_helper.rb
@@ -0,0 +1,2 @@
+module Teachers::QuizzesHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,30 @@
 Rails.application.routes.draw do
   namespace :teachers do
+    get 'quizzes/index'
+  end
+
+  namespace :teachers do
+    get 'quizzes/show'
+  end
+
+  namespace :teachers do
+    get 'quizzes/new'
+  end
+
+  namespace :teachers do
+    get 'quizzes/destroy'
+  end
+
+  namespace :teachers do
+    get 'quizzes/edit'
+  end
+
+  namespace :teachers do
     resources :exams, only: [:index]
+  end
+
+  namespace :teachers do
+    resources :quizzes
   end
 
   devise_for :users
