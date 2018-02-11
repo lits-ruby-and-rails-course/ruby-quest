de362a159bae03ca34d6ec592e73c1f73c3d7c73
kmandziy <kmandzii@gmail.com>
2018-02-11 21:35:49 +0200

    Commit

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,6 @@ ActiveRecord::Schema.define(version: 20180202164822) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "answers", force: :cascade do |t|
-    t.text "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "questions", force: :cascade do |t|
     t.text "text"
     t.datetime "created_at", null: false
